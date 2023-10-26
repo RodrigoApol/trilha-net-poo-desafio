640db59164dc84cd79e93ca4a5daebe85d139a82
Rodrigo Apolinário <rodrigo_ap2001@outlook.com>
2023-10-26 17:04:41 -0300

    resolvendo o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new(numero: "(51) 98888-8888", modelo: "X1-01", imei: "21248484", memoria: 64);
+Iphone iphone = new("(51) 97777-7777", "5s", "555115115", 128);
+
+// Métodos em comum
+nokia.Ligar();
+iphone.Ligar();
+Console.WriteLine("");
+nokia.ReceberLigacao();
+iphone.ReceberLigacao();
+Console.WriteLine("");
+
+// Métodos sobrescritos
+nokia.InstalarAplicativo("WhatsApp");
+iphone.InstalarAplicativo("HBO Max");
+Console.WriteLine("");
